ed19c3aa897c96f40f0234c90d9bf6ac1ab22728
Fredrik Borg <fredrikb@mnemonic.no>
2022-09-14 18:38:55 +0200

    initial import

---
+++ pylsp_autoimport/__about__.py
@@ -0,0 +1,10 @@
+__title__ = "pylsp-autoimport"
+__version__ = "0.1.0"
+
+__summary__ = "Auto Import plugin for python-lsp-server"
+__uri__ = "https://github.com/frbor/pylsp-autoimport"
+__author__ = "Fredrik Borg"
+__email__ = "fredrik.borg@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 %s" % __author__
